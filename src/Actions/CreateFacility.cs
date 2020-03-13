dbabbfb15a79b4e5e2a281319a5e2587eeceb2a7
namitamanohar <manohar.namita@gmail.com>
2020-03-13 09:23:44 -0500

    some comments to figure out what is going on

--- src/Actions/CreateFacility.cs
+++ src/Actions/CreateFacility.cs
@@ -8,6 +8,9 @@ namespace Trestlebridge.Actions {
         public static void CollectInput (Farm farm) {
             Console.WriteLine ("1. Grazing field");
             Console.WriteLine ("2. Plowed field");
+            Console.WriteLine ("3. Natural field");
+            Console.WriteLine ("4. Chicken house");
+            Console.WriteLine ("5. Duck house");
 
             Console.WriteLine ();
             Console.WriteLine ("Choose what you want to create");
@@ -15,10 +18,9 @@ namespace Trestlebridge.Actions {
             Console.Write ("> ");
             string input = Console.ReadLine ();
 
-            switch (Int32.Parse(input))
-            {
+            switch (Int32.Parse (input)) {
                 case 1:
-                    farm.AddGrazingField(new GrazingField());
+                    farm.AddGrazingField (new GrazingField ());
                     break;
                 default:
                     break;
